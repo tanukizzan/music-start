88e07575355fabc3e6c46219371047837987af17
tanukizzan <51444253+tanukizzan@users.noreply.github.com>
2020-11-23 12:27:50 +0900

    完成

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# rhythm-music
+# Music Start!
 
-開発中...
+https://app.tanukizzan.com/music-start/
+
+音楽ファイル（.mp3, .m4aなど）を読み込むと曲に合わせて画面が動き出すサイトです。
+
+画面が動く部分は[Rythm.js](http://okazari.github.io/Rythm.js/)を使用しています。
+
+推奨環境は最新のGoogle Chromeです。またPCのみ対応しています。
+
+※違う曲をかけたい場合はページを再読み込みしてください。
--- script.js
+++ script.js
@@ -1,77 +1,44 @@
 const inputFile = document.getElementById('file');
 const music = document.getElementById('music');
 
-const canvas = document.getElementById('sample');
-if (canvas.getContext) {
-  const context = canvas.getContext('2d');
-  // TODO かっこいい図形作る
-  context.fillRect(20, 20, 100, 100);
-  context.fill();
-}
-
 inputFile.addEventListener('change', function (e) {
   const file = e.target.files;
   const reader = new FileReader();
   reader.readAsDataURL(file[0]);
-  reader.onload = function () {
-    // music.src = reader.result;
+  reader.onload = () => {
+    music.src = reader.result;
     const rythm = new Rythm();
     rythm.setMusic(reader.result);
-    // rythm.addRythm('rythm-bass', 'neon', 0, 10, {
-    //   from: [0,0,255],
-    //   to:[255,0,255]
-    // });
-    // rythm.addRythm('rythm-bass', 'borderColor', 0, 10);
-    rythm.start();
-  }
-})
-
-/* ビジュアライザー
-window.onload = function(){
-  var source, animationId;
-  const audioContext = new AudioContext;
-  const fileReader   = new FileReader;
-　
-  const analyser = audioContext.createAnalyser();
-  analyser.fftSize = 128;
-  analyser.connect(audioContext.destination);
-　
-  const canvas        = document.getElementById('visualizer');
-  const canvasContext = canvas.getContext('2d');
-  canvas.setAttribute('width', analyser.frequencyBinCount * 10);
-　
-  fileReader.onload = function(){
-    audioContext.decodeAudioData(fileReader.result, function(buffer){
-      if(source) {
-        source.stop();
-        cancelAnimationFrame(animationId);
-      }
-　
-      source = audioContext.createBufferSource();
-　
-      source.buffer = buffer;
-      source.connect(analyser);
-      source.start(0);
-　
-      animationId = requestAnimationFrame(render);
+    rythm.connectExternalAudioElement(music);
+    rythm.addRythm('jump', 'jump', 150, 40, {min: -20, max: 20});
+    rythm.addRythm('jump2', 'jump', 150, 40, {min: 20, max: -20});
+    rythm.addRythm('twist', 'twist', 0, 10);
+    rythm.addRythm('twist2', 'twist', 0, 10, {direction: 'left'});
+    rythm.addRythm('tilt', 'tilt', 0, 10);
+    rythm.addRythm('tilt2', 'tilt', 0, 10, {reverse : true});
+    rythm.addRythm('swing', 'swing', 0, 10, {direction: 'left'});
+    rythm.addRythm('pulse', 'pulse', 0, 10, {min: 1, max: 1.75});
+    rythm.addRythm('neon', 'neon', 0, 10, {
+      from: [0,0,255],
+      to:[255,0,255]
     });
-  };
-　
-  inputFile.addEventListener('change', function(e){
-    fileReader.readAsArrayBuffer(e.target.files[0]);
-  });
-　
-  render = function(){
-    var spectrums = new Uint8Array(analyser.frequencyBinCount);
-    analyser.getByteFrequencyData(spectrums);
-　
-    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-    for(var i=0, len=spectrums.length; i<len; i++){
-      canvasContext.fillRect(i*10, 0, 5, spectrums[i]);
-    }
-　
-    animationId = requestAnimationFrame(render);
-  };
-};
-
-*/
+    rythm.addRythm('fontColor1', 'fontColor', 0, 10, {
+      from: [255,0,255],
+      to:[0,0,255]
+    });
+    rythm.addRythm('fontColor2', 'fontColor', 0, 10, {
+      from: [0,255,0],
+      to:[255,255,0]
+    });
+    rythm.addRythm('color1', 'color', 0, 10, {
+      from: [0,0,255],
+      to:[255,0,255]
+    });
+    rythm.addRythm('color2', 'color', 0, 10, {
+      from: [0,255,0],
+      to:[255,255,0]
+    });
+    rythm.start();
+    music.play();
+  }
+});
